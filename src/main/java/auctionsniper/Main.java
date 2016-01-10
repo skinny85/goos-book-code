6e52afd7286b0a5fdd7dbe8d45d0ba3a65c2328a
Adam Ruka <adamruka85@gmail.com>
2016-01-10 23:37:32 +0100

    First end-to-end test - Red (connecting to the XMPP server from Main, the test is failing as in the book, page 100 - "check that its label text is "Lost", but... label text was "Joining"").

--- src/main/java/auctionsniper/Main.java
+++ src/main/java/auctionsniper/Main.java
@@ -1,6 +1,11 @@
 package auctionsniper;
 
 import auctionsniper.ui.MainWindow;
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
 
 import javax.swing.*;
 
@@ -9,14 +14,48 @@ public class Main {
     public static final String STATUS_LOST = "Lost";
     public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
 
+    private static final int ARG_HOSTNAME = 0;
+    private static final int ARG_USERNAME = 1;
+    private static final int ARG_PASSWORD = 2;
+    private static final int ARG_ITEM_ID = 3;
+
+    public static final String AUCTION_RESOURCE = "Auction";
+    public static final String ITEM_ID_AS_LOGIN = "auction-%s";
+    public static final String AUCTION_ID_FORMAT =
+            ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE;
+
+    public static void main(String... args) throws Exception {
+        Main main = new Main();
+        XMPPConnection connection = connectTo(args[ARG_HOSTNAME],
+                args[ARG_USERNAME],
+                args[ARG_PASSWORD]);
+        Chat chat = connection.getChatManager().createChat(
+                auctionId(args[ARG_ITEM_ID], connection),
+                new MessageListener() {
+                    public void processMessage(Chat aChat, Message message) {
+                        // nothing yet
+                    }
+                });
+        chat.sendMessage(new Message());
+    }
+
+    private static XMPPConnection connectTo(String hostname, String username, String password)
+            throws XMPPException {
+        XMPPConnection connection = new XMPPConnection(hostname);
+        connection.connect();
+        connection.login(username, password, AUCTION_RESOURCE);
+        return connection;
+    }
+
+    private static String auctionId(String itemId, XMPPConnection connection) {
+        return String.format(AUCTION_ID_FORMAT, itemId,
+                connection.getServiceName());
+    }
+
     private MainWindow ui;
 
     public Main() throws Exception {
         startUserInterface();
-    }
-
-    public static void main(String... args) throws Exception {
-        Main main = new Main();
     }
 
     private void startUserInterface() throws Exception {
