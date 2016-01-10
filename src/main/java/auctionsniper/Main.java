e131ad77883f009ba9ef3b503ded6939fa3044ae
Adam Ruka <adamruka85@gmail.com>
2016-01-10 23:03:06 +0100

    First end-to-end test - Red (implemented Main and added MainWindow, the test is failing as in the book, page 97 - "contained 0 JLabel (with name "sniper status")").

--- src/main/java/auctionsniper/Main.java
+++ src/main/java/auctionsniper/Main.java
@@ -1,12 +1,30 @@
 package auctionsniper;
 
+import auctionsniper.ui.MainWindow;
+
+import javax.swing.*;
+
 public class Main {
     public static final String STATUS_JOINING = "Joining";
     public static final String STATUS_LOST = "Lost";
     public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
     public static final String SNIPER_STATUS_NAME = "sniper status";
 
-    public static void main(String... args) {
+    private MainWindow ui;
 
+    public Main() throws Exception {
+        startUserInterface();
+    }
+
+    public static void main(String... args) throws Exception {
+        Main main = new Main();
+    }
+
+    private void startUserInterface() throws Exception {
+        SwingUtilities.invokeAndWait(new Runnable() {
+            public void run() {
+                ui = new MainWindow();
+            }
+        });
     }
 }
