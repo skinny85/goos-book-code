3e0204ac0a6e875bef6b63af31c76f7f872f5b79
Adam Ruka <adamruka85@gmail.com>
2016-01-10 01:15:19 +0100

    First end-to-end test - Red (stub helper classes only).

---
+++ src/test/java/test/endtoend/auctionsniper/FakeAuctionServer.java
@@ -0,0 +1,23 @@
+package test.endtoend.auctionsniper;
+
+public class FakeAuctionServer {
+    public FakeAuctionServer(String itemId) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void startSellingItem() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void hasReceivedJoinRequestFromSniper() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void announceClosed() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void stop() {
+        throw new UnsupportedOperationException();
+    }
+}
