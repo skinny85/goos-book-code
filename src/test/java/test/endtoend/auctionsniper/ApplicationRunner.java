a6094ebe66fb044afacdd2c01974b33cf4308ecd
Adam Ruka <adamruka85@gmail.com>
2016-01-10 01:15:19 +0100

    First end-to-end test - Red (stub helper classes only).

---
+++ src/test/java/test/endtoend/auctionsniper/ApplicationRunner.java
@@ -0,0 +1,15 @@
+package test.endtoend.auctionsniper;
+
+public class ApplicationRunner {
+    public void startBiddingIn(FakeAuctionServer auction) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void showsSniperHasLostAuction() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void stop() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/test/java/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
@@ -0,0 +1,29 @@
+package test.endtoend.auctionsniper;
+
+import org.junit.After;
+import org.junit.Test;
+
+public class AuctionSniperEndToEndTest {
+    private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+    private final ApplicationRunner application = new ApplicationRunner();
+
+    @Test
+    public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
+        auction.startSellingItem();
+        application.startBiddingIn(auction);
+        auction.hasReceivedJoinRequestFromSniper();
+        auction.announceClosed();
+        application.showsSniperHasLostAuction();
+    }
+
+    // Additional cleanup
+    @After
+    public void stopAuction() {
+        auction.stop();
+    }
+
+    @After
+    public void stopApplication() {
+        application.stop();
+    }
+}
