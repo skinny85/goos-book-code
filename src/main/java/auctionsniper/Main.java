3e9909bc26d53cf567ed892c0fa4255b468a4819
Adam Ruka <adamruka85@gmail.com>
2016-01-10 23:16:59 +0100

    First end-to-end test - Red (added sniperStatus Label to the MainWindow, the test is failing as in the book, page 98 - "Expected: is not null, got: null").

--- src/main/java/auctionsniper/Main.java
+++ src/main/java/auctionsniper/Main.java
@@ -8,7 +8,6 @@ public class Main {
     public static final String STATUS_JOINING = "Joining";
     public static final String STATUS_LOST = "Lost";
     public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
-    public static final String SNIPER_STATUS_NAME = "sniper status";
 
     private MainWindow ui;
 
--- src/main/java/auctionsniper/ui/MainWindow.java
+++ src/main/java/auctionsniper/ui/MainWindow.java
@@ -1,14 +1,30 @@
 package auctionsniper.ui;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+import static auctionsniper.Main.STATUS_JOINING;
 
 public class MainWindow extends JFrame {
     public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
+    public static final String SNIPER_STATUS_NAME = "sniper status";
+
+    private final JLabel sniperStatus = createLabel(STATUS_JOINING);
 
     public MainWindow() {
         super("Auction Sniper");
         setName(MAIN_WINDOW_NAME);
+        add(sniperStatus);
+        pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    private static JLabel createLabel(String initialText) {
+        JLabel result = new JLabel(initialText);
+        result.setName(SNIPER_STATUS_NAME);
+        result.setBorder(new LineBorder(Color.BLACK));
+        return result;
     }
 }
