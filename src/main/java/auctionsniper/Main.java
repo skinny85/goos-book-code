0b5d6e3ce62dff3ae2861fd64b056edf19af9a18
Adam Ruka <adamruka85@gmail.com>
2016-01-10 01:56:17 +0100

    First end-to-end test - Red (implemented ApplicationRunner and FakeAuctionServer, the test is failing as in the book, page 96 - "all top level windows contained 0 JFrame (with name "Auction Sniper Main" and showing on screen)").

---
+++ src/main/java/auctionsniper/Main.java
@@ -0,0 +1,12 @@
+package auctionsniper;
+
+public class Main {
+    public static final String STATUS_JOINING = "Joining";
+    public static final String STATUS_LOST = "Lost";
+    public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
+    public static final String SNIPER_STATUS_NAME = "sniper status";
+
+    public static void main(String... args) {
+
+    }
+}
