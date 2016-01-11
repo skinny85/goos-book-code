b6a9a3a8734e1f20e697eeda59f45583802ddfb7
Adam Ruka <adamruka85@gmail.com>
2016-01-11 23:25:21 +0100

    First unit test ("AuctionMessageTranslator - notifies auction closed when message received") - Green (page 116).

--- src/main/java/auctionsniper/AuctionMessageTranslator.java
+++ src/main/java/auctionsniper/AuctionMessageTranslator.java
@@ -1,15 +1,18 @@
 package auctionsniper;
 
 import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.packet.Message;
 
-public class AuctionMessageTranslator {
+public class AuctionMessageTranslator implements MessageListener {
     private final AuctionEventListener listener;
 
     public AuctionMessageTranslator(AuctionEventListener listener) {
         this.listener = listener;
     }
 
+    @Override
     public void processMessage(Chat chat, Message message) {
+        listener.auctionClosed();
     }
 }
