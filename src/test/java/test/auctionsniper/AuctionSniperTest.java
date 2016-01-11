869fe4363ebb5949c33e0ff3652fb5bf98dba41e
Adam Ruka <adamruka85@gmail.com>
2016-01-12 00:07:25 +0100

    Third unit test ("AuctionSniper - reports lost when auction closes") - Red (page 124).

---
+++ src/test/java/test/auctionsniper/AuctionSniperTest.java
@@ -0,0 +1,26 @@
+package test.auctionsniper;
+
+import auctionsniper.AuctionSniper;
+import auctionsniper.SniperListener;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JMock.class)
+public class AuctionSniperTest {
+    private final Mockery context = new Mockery();
+    private final SniperListener sniperListener =
+            context.mock(SniperListener.class);
+    private final AuctionSniper sniper = new AuctionSniper(sniperListener);
+
+    @Test
+    public void reportsLostWhenAuctionCloses() {
+        context.checking(new Expectations() {{
+            one(sniperListener).sniperLost();
+        }});
+
+        sniper.auctionClosed();
+    }
+}
