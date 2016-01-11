b8e037b91ee671ef5374733a75a33e07b64cb4c0
Adam Ruka <adamruka85@gmail.com>
2016-01-11 23:58:25 +0100

    Second unit test ("AuctionMessageTranslator - notifies bid details when current price message received") - Green (page 120).

--- src/test/java/test/endtoend/auctionsniper/FakeAuctionServer.java
+++ src/test/java/test/endtoend/auctionsniper/FakeAuctionServer.java
@@ -72,7 +72,7 @@ public class FakeAuctionServer {
     }
 
     public void announceClosed() throws XMPPException {
-        currentChat.sendMessage(new Message());
+        currentChat.sendMessage("SOLVersion: 1.1; Event: CLOSE;");
     }
 
     public void stop() {
