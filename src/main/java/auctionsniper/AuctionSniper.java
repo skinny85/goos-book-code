7dd9e632fcd5d5e5d8c61d1c3b650d248ecac96e
Adam Ruka <adamruka85@gmail.com>
2016-01-12 00:10:46 +0100

    Third unit test ("AuctionSniper - reports lost when auction closes") - Green (page 124).

--- src/main/java/auctionsniper/AuctionSniper.java
+++ src/main/java/auctionsniper/AuctionSniper.java
@@ -1,9 +1,17 @@
 package auctionsniper;
 
-public class AuctionSniper {
+public class AuctionSniper implements AuctionEventListener {
+    private final SniperListener sniperListener;
+
     public AuctionSniper(SniperListener sniperListener) {
+        this.sniperListener = sniperListener;
     }
 
     public void auctionClosed() {
+        sniperListener.sniperLost();
+    }
+
+    @Override
+    public void currentPrice(int price, int increment) {
     }
 }
