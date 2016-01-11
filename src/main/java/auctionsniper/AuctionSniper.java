aba17368caaeeac2fcb1e0b6d8da01765edf5fb3
Adam Ruka <adamruka85@gmail.com>
2016-01-12 00:33:15 +0100

    Fourth unit test ("AuctionSniper - bids higher and reports bidding when new price arrives") - Green (page 128).

--- src/main/java/auctionsniper/AuctionSniper.java
+++ src/main/java/auctionsniper/AuctionSniper.java
@@ -1,9 +1,11 @@
 package auctionsniper;
 
 public class AuctionSniper implements AuctionEventListener {
+    private final Auction auction;
     private final SniperListener sniperListener;
 
     public AuctionSniper(Auction auction, SniperListener sniperListener) {
+        this.auction = auction;
         this.sniperListener = sniperListener;
     }
 
@@ -13,5 +15,7 @@ public class AuctionSniper implements AuctionEventListener {
 
     @Override
     public void currentPrice(int price, int increment) {
+        auction.bid(price + increment);
+        sniperListener.sniperBidding();
     }
 }
