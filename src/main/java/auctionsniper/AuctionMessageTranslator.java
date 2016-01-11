b780d9b7ca4b4380f3bdb4a4c12a71336bdc7862
Adam Ruka <adamruka85@gmail.com>
2016-01-11 23:58:25 +0100

    Second unit test ("AuctionMessageTranslator - notifies bid details when current price message received") - Green (page 120).

--- src/main/java/auctionsniper/AuctionMessageTranslator.java
+++ src/main/java/auctionsniper/AuctionMessageTranslator.java
@@ -4,6 +4,8 @@ import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.packet.Message;
 
+import java.util.HashMap;
+
 public class AuctionMessageTranslator implements MessageListener {
     private final AuctionEventListener listener;
 
@@ -11,8 +13,23 @@ public class AuctionMessageTranslator implements MessageListener {
         this.listener = listener;
     }
 
-    @Override
     public void processMessage(Chat chat, Message message) {
-        listener.auctionClosed();
+        HashMap<String, String> event = unpackEventFrom(message);
+        String type = event.get("Event");
+        if ("CLOSE".equals(type)) {
+            listener.auctionClosed();
+        } else if ("PRICE".equals(type)) {
+            listener.currentPrice(Integer.parseInt(event.get("CurrentPrice")),
+                    Integer.parseInt(event.get("Increment")));
+        }
+    }
+
+    private HashMap<String, String> unpackEventFrom(Message message) {
+        HashMap<String, String> event = new HashMap<String, String>();
+        for (String element : message.getBody().split(";")) {
+            String[] pair = element.split(":");
+            event.put(pair[0].trim(), pair[1].trim());
+        }
+        return event;
     }
 }
