6ee7f4b36902a1dcc9f7dd396222aa170b7c747b
Adam Ruka <adamruka85@gmail.com>
2016-01-14 00:15:13 +0100

    New AuctionMessageTranslator unit test - Red (introducing AuctionEventListener.PriceSource, page 141).

--- src/main/java/auctionsniper/AuctionEventListener.java
+++ src/main/java/auctionsniper/AuctionEventListener.java
@@ -1,7 +1,11 @@
 package auctionsniper;
 
 public interface AuctionEventListener {
+    enum PriceSource {
+        FromSniper, FromOtherBidder;
+    }
+
     void auctionClosed();
 
-    void currentPrice(int price, int increment);
+    void currentPrice(int price, int increment, PriceSource priceSource);
 }
