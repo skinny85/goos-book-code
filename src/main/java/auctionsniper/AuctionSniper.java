260601456099607bbae5defb81f2f62ebd4403b5
Adam Ruka <adamruka85@gmail.com>
2016-01-16 13:39:06 +0100

    New Auction Sniper unit test ("reports won if auction closes when winning") - Green (page 147).

--- src/main/java/auctionsniper/AuctionSniper.java
+++ src/main/java/auctionsniper/AuctionSniper.java
@@ -3,6 +3,7 @@ package auctionsniper;
 public class AuctionSniper implements AuctionEventListener {
     private final Auction auction;
     private final SniperListener sniperListener;
+    private boolean isWinning = false;
 
     public AuctionSniper(Auction auction, SniperListener sniperListener) {
         this.auction = auction;
@@ -10,19 +11,22 @@ public class AuctionSniper implements AuctionEventListener {
     }
 
     public void auctionClosed() {
-        sniperListener.sniperLost();
+        if (isWinning) {
+            sniperListener.sniperWon();
+        } else {
+            sniperListener.sniperLost();
+        }
     }
 
     @Override
     public void currentPrice(int price, int increment, PriceSource priceSource) {
-        switch (priceSource) {
-            case FromSniper:
-                sniperListener.sniperWinning();
-                break;
-            case FromOtherBidder:
-                auction.bid(price + increment);
-                sniperListener.sniperBidding();
-                break;
+        isWinning = priceSource == PriceSource.FromSniper;
+
+        if (isWinning) {
+            sniperListener.sniperWinning();
+        } else {
+            auction.bid(price + increment);
+            sniperListener.sniperBidding();
         }
     }
 }
