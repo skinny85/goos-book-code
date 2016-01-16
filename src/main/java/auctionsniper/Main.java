a824790d6773c4a71f32578fa03baa39134977eb
Adam Ruka <adamruka85@gmail.com>
2016-01-16 13:44:06 +0100

    Third end-to-end test ("Sniper wins an auction by bidding higher") - Green (expected label to be "Winning" but was "Bidding", page 147).

--- src/main/java/auctionsniper/Main.java
+++ src/main/java/auctionsniper/Main.java
@@ -99,10 +99,12 @@ public class Main{
 
         @Override
         public void sniperWinning() {
+            showStatus(MainWindow.STATUS_WINNING);
         }
 
         @Override
         public void sniperWon() {
+            showStatus(MainWindow.STATUS_WON);
         }
 
         private void showStatus(final String status) {
