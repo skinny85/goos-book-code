447a6de4fab0e90a69838c0bf0853297f2980179
Adam Ruka <adamruka85@gmail.com>
2016-01-17 16:00:19 +0100

    Showing more Auction details in the Table than just status (item ID, last price, last bid) - Red (page 153).

--- src/test/java/test/endtoend/auctionsniper/AuctionSniperDriver.java
+++ src/test/java/test/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -6,7 +6,9 @@ import com.objogate.wl.swing.driver.JFrameDriver;
 import com.objogate.wl.swing.driver.JTableDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 
+import static com.objogate.wl.swing.matcher.IterableComponentsMatcher.matching;
 import static com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText;
+import static java.lang.String.valueOf;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class AuctionSniperDriver extends JFrameDriver {
@@ -18,7 +20,16 @@ public class AuctionSniperDriver extends JFrameDriver {
                 new AWTEventQueueProber(timeoutMillis, 100));
     }
 
-    public void showsSniperStatus(String statusText) {
+    public void showsSniperStatus(String itemId, String statusText) {
         new JTableDriver(this).hasCell(withLabelText(equalTo(statusText)));
+    }
+
+    public void showsSniperStatus(String itemId, int lastPrice, int lastBid, String statusText) {
+        JTableDriver table = new JTableDriver(this);
+        table.hasRow(matching(
+                withLabelText(itemId),
+                withLabelText(valueOf(lastPrice)),
+                withLabelText(valueOf(lastBid)),
+                withLabelText(statusText)));
     }
 }
--- src/test/java/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
+++ src/test/java/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
@@ -26,7 +26,7 @@ public class AuctionSniperEndToEndTest {
         auction.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
 
         auction.reportPrice(1000, 98, "other bidder");
-        application.hasShownSniperIsBidding();
+        application.hasShownSniperIsBidding(1000, 1098);
 
         auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
 
@@ -42,15 +42,15 @@ public class AuctionSniperEndToEndTest {
         auction.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
 
         auction.reportPrice(1000, 98, "other bidder");
-        application.hasShownSniperIsBidding();
+        application.hasShownSniperIsBidding(1000, 1098);
 
         auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
 
         auction.reportPrice(1098, 97, ApplicationRunner.SNIPER_XMPP_ID);
-        application.hasShownSniperIsWinning();
+        application.hasShownSniperIsWinning(1098);
 
         auction.announceClosed();
-        application.showsSniperHasWonAuction();
+        application.showsSniperHasWonAuction(1098);
     }
 
     // Additional cleanup
