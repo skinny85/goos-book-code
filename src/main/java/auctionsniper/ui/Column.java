c7ba0d750d60c2f3ab0d0e003c7623e8880b1b6a
Adam Ruka <adamruka85@gmail.com>
2016-01-17 23:44:43 +0100

    Adding column titles - Red (add the name attribute to Column, page 170).

--- src/main/java/auctionsniper/ui/Column.java
+++ src/main/java/auctionsniper/ui/Column.java
@@ -4,30 +4,36 @@ import auctionsniper.SniperSnapshot;
 import auctionsniper.ui.MainWindow.SnipersTableModel;
 
 public enum Column {
-    ITEM_IDENTIFIER {
+    ITEM_IDENTIFIER("Item") {
         @Override
         public Object valueIn(SniperSnapshot snapshot) {
             return snapshot.itemId;
         }
     },
-    LAST_PRICE {
+    LAST_PRICE("Last Price") {
         @Override
         public Object valueIn(SniperSnapshot snapshot) {
             return snapshot.lastPrice;
         }
     },
-    LAST_BID {
+    LAST_BID("Last Bid") {
         @Override
         public Object valueIn(SniperSnapshot snapshot) {
             return snapshot.lastBid;
         }
     },
-    SNIPER_STATE {
+    SNIPER_STATE("State") {
         @Override
         public Object valueIn(SniperSnapshot snapshot) {
             return SnipersTableModel.textFor(snapshot.state);
         }
     };
+
+    public final String name;
+
+    private Column(String name) {
+        this.name = name;
+    }
 
     public abstract Object valueIn(SniperSnapshot snapshot);
 
