7d10b88a858fe3ef2dae0236a09181bea0b01d8a
Adam Ruka <adamruka85@gmail.com>
2016-01-17 16:56:27 +0100

    SnipersTableModel uni tests - Green (page 158).

--- src/main/java/auctionsniper/ui/MainWindow.java
+++ src/main/java/auctionsniper/ui/MainWindow.java
@@ -48,19 +48,25 @@ public class MainWindow extends JFrame {
     }
 
     public static class SnipersTableModel extends AbstractTableModel {
+        private final static SniperState STARTING_UP = new SniperState("", 0, 0);
+
         private String statusText = STATUS_JOINING;
+        private SniperState sniperState = STARTING_UP;
 
         public void setStatusText(String newStatusText) {
             statusText = newStatusText;
             fireTableRowsUpdated(0, 0);
         }
 
-        public void sniperStatusChanged(SniperState sniperState, String statusText) {
-            throw new UnsupportedOperationException();
+        public void sniperStatusChanged(SniperState newSniperState,
+                                        String newStatusText) {
+            sniperState = newSniperState;
+            statusText = newStatusText;
+            fireTableRowsUpdated(0, 0);
         }
 
         public int getColumnCount() {
-            return 1;
+            return Column.values().length;
         }
 
         public int getRowCount() {
@@ -68,7 +74,18 @@ public class MainWindow extends JFrame {
         }
 
         public Object getValueAt(int rowIndex, int columnIndex) {
-            return statusText;
+            switch (Column.at(columnIndex)) {
+                case ITEM_IDENTIFIER:
+                    return sniperState.itemId;
+                case LAST_PRICE:
+                    return sniperState.lastPrice;
+                case LAST_BID:
+                    return sniperState.lastBid;
+                case SNIPER_STATUS:
+                    return statusText;
+                default:
+                    throw new IllegalArgumentException("No column at " + columnIndex);
+            }
         }
     }
 }
