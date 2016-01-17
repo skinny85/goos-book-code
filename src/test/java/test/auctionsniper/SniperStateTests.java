cf0d1e4b938f5f5250f6328918dcf3e2c85ea057
Adam Ruka <adamruka85@gmail.com>
2016-01-17 20:20:31 +0100

    SniperState unit test - Red (page 165).

--- src/test/java/test/auctionsniper/SniperStateTests.java
+++ src/test/java/test/auctionsniper/SniperStateTests.java
@@ -1,30 +1,26 @@
 package test.auctionsniper;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
 import auctionsniper.SniperState;
 import auctionsniper.util.Defect;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class SniperStateTests {
+    @Test
+    public void isWonWhenAuctionClosesWhileWinning() {
+        assertEquals(SniperState.LOST, SniperState.JOINING.whenAuctionClosed());
+        assertEquals(SniperState.LOST, SniperState.BIDDING.whenAuctionClosed());
+        assertEquals(SniperState.WON, SniperState.WINNING.whenAuctionClosed());
+    }
 
-  @Test
-  public void
-  isWonWhenAuctionClosesWhileWinning() {
-    assertEquals(SniperState.LOST, SniperState.JOINING.whenAuctionClosed());
-    assertEquals(SniperState.LOST, SniperState.BIDDING.whenAuctionClosed());
-    assertEquals(SniperState.WON, SniperState.WINNING.whenAuctionClosed());
-  }
-  
-  @Test(expected=Defect.class) public void
-  defectIfAuctionClosesWhenWon() {
-    SniperState.WON.whenAuctionClosed();
-  }
+    @Test(expected = Defect.class)
+    public void defectIfAuctionClosesWhenWon() {
+        SniperState.WON.whenAuctionClosed();
+    }
 
-  @Test(expected=Defect.class) public void
-  defectIfAuctionClosesWhenLost() {
-    SniperState.LOST.whenAuctionClosed();
-  }
-
+    @Test(expected = Defect.class)
+    public void defectIfAuctionClosesWhenLost() {
+        SniperState.LOST.whenAuctionClosed();
+    }
 }
