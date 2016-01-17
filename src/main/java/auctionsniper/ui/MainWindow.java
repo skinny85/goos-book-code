5e3a7855d528e0db54e39f77b55c8afbd528a5ec
Adam Ruka <adamruka85@gmail.com>
2016-01-17 15:14:19 +0100

    Changing from Label to Table - Green (changed the MainWindow to use a JTable instead of a JLabel, page 151).

--- src/main/java/auctionsniper/ui/MainWindow.java
+++ src/main/java/auctionsniper/ui/MainWindow.java
@@ -1,38 +1,64 @@
 package auctionsniper.ui;
 
 import javax.swing.*;
-import javax.swing.border.LineBorder;
+import javax.swing.table.AbstractTableModel;
 import java.awt.*;
 
 import static auctionsniper.Main.STATUS_JOINING;
 
 public class MainWindow extends JFrame {
     public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
-    public static final String SNIPER_STATUS_NAME = "sniper status";
+    private static final String SNIPERS_TABLE_NAME = "Snipers Table";
     public static final String STATUS_LOST = "Lost";
     public static final String STATUS_BIDDING = "Bidding";
     public static final String STATUS_WINNING = "Winning";
     public static final String STATUS_WON = "Won";
 
-    private final JLabel sniperStatus = createLabel(STATUS_JOINING);
+    private final SnipersTableModel snipers = new SnipersTableModel();
 
     public MainWindow() {
         super("Auction Sniper");
         setName(MAIN_WINDOW_NAME);
-        add(sniperStatus);
+        fillContentPane(makeSnipersTable());
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
-    private static JLabel createLabel(String initialText) {
-        JLabel result = new JLabel(initialText);
-        result.setName(SNIPER_STATUS_NAME);
-        result.setBorder(new LineBorder(Color.BLACK));
-        return result;
+    public void showStatus(String statusText) {
+        snipers.setStatusText(statusText);
     }
 
-    public void showStatus(String status) {
-        sniperStatus.setText(status);
+    private JTable makeSnipersTable() {
+        final JTable snipersTable = new JTable(snipers);
+        snipersTable.setName(SNIPERS_TABLE_NAME);
+        return snipersTable;
+    }
+
+    private void fillContentPane(JTable snipersTable) {
+        final Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+        contentPane.add(new JScrollPane(snipersTable), BorderLayout.CENTER);
+    }
+
+    public class SnipersTableModel extends AbstractTableModel {
+        private String statusText = STATUS_JOINING;
+
+        public void setStatusText(String newStatusText) {
+            statusText = newStatusText;
+            fireTableRowsUpdated(0, 0);
+        }
+
+        public int getColumnCount() {
+            return 1;
+        }
+
+        public int getRowCount() {
+            return 1;
+        }
+
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            return statusText;
+        }
     }
 }
