3643bf5356a5b2f90a6698e4347109964973339c
Adam Ruka <adamruka85@gmail.com>
2016-01-17 22:11:12 +0100

    Column unit tests - Green (page 167).

--- src/main/java/auctionsniper/ui/Column.java
+++ src/main/java/auctionsniper/ui/Column.java
@@ -1,16 +1,35 @@
 package auctionsniper.ui;
 
 import auctionsniper.SniperSnapshot;
+import auctionsniper.ui.MainWindow.SnipersTableModel;
 
 public enum Column {
-    ITEM_IDENTIFIER,
-    LAST_PRICE,
-    LAST_BID,
-    SNIPER_STATE;
+    ITEM_IDENTIFIER {
+        @Override
+        public Object valueIn(SniperSnapshot snapshot) {
+            return snapshot.itemId;
+        }
+    },
+    LAST_PRICE {
+        @Override
+        public Object valueIn(SniperSnapshot snapshot) {
+            return snapshot.lastPrice;
+        }
+    },
+    LAST_BID {
+        @Override
+        public Object valueIn(SniperSnapshot snapshot) {
+            return snapshot.lastBid;
+        }
+    },
+    SNIPER_STATE {
+        @Override
+        public Object valueIn(SniperSnapshot snapshot) {
+            return SnipersTableModel.textFor(snapshot.state);
+        }
+    };
 
-    public Object valueIn(SniperSnapshot snapshot) {
-        throw new UnsupportedOperationException();
-    }
+    public abstract Object valueIn(SniperSnapshot snapshot);
 
     public static Column at(int offset) {
         return values()[offset];
