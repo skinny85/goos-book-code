44cdc03186d92cc1e6f16e509dbc050ee933499d
Adam Ruka <adamruka85@gmail.com>
2016-01-18 23:07:26 +0100

    Chapter 16 (Sniping for multiple items).
    New end-to-end test ("Sniper bids for mutliple items") - Red (wrote the test and changed startBiddingIn in ApplicationRunner to accept a varargs number of FakeAuctionServers as arguments, page 176).

--- src/test/java/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
+++ src/test/java/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class AuctionSniperEndToEndTest {
     private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+    private final FakeAuctionServer auction2 = new FakeAuctionServer("item-65432");
     private final ApplicationRunner application = new ApplicationRunner();
 
     @Test
@@ -51,6 +52,34 @@ public class AuctionSniperEndToEndTest {
 
         auction.announceClosed();
         application.showsSniperHasWonAuction(auction, 1098);
+    }
+
+    @Test
+    public void sniperBidsForMultipleItems() throws Exception {
+        auction.startSellingItem();
+        auction2.startSellingItem();
+
+        application.startBiddingIn(auction, auction2);
+        auction.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
+        auction2.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
+
+        auction.reportPrice(1000, 98, "other bidder");
+        auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
+
+        auction2.reportPrice(500, 21, "other bidder");
+        auction2.hasReceivedBid(521, ApplicationRunner.SNIPER_XMPP_ID);
+
+        auction.reportPrice(1098, 97, ApplicationRunner.SNIPER_XMPP_ID);
+        auction2.reportPrice(521, 22, ApplicationRunner.SNIPER_XMPP_ID);
+
+        application.hasShownSniperIsWinning(auction, 1098);
+        application.hasShownSniperIsWinning(auction2, 521);
+
+        auction.announceClosed();
+        auction2.announceClosed();
+
+        application.showsSniperHasWonAuction(auction, 1098);
+        application.showsSniperHasWonAuction(auction2, 521);
     }
 
     // Additional cleanup
