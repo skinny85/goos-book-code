cd038623c154c87c298dd1444321c1bbe3876035
Adam Ruka <adamruka85@gmail.com>
2016-01-19 23:40:19 +0100

    Adding items through the user interface - Red (updating the tests, page 184).

--- src/main/java/auctionsniper/ui/MainWindow.java
+++ src/main/java/auctionsniper/ui/MainWindow.java
@@ -17,6 +17,8 @@ public class MainWindow extends JFrame {
     public static final String APPLICATION_TITLE = "Auction Sniper";
     public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
     private static final String SNIPERS_TABLE_NAME = "Snipers Table";
+    public static final String NEW_ITEM_ID_NAME = "item id";
+    public static final String JOIN_BUTTON_NAME = "join button";
 
     private final SnipersTableModel snipers;
 
