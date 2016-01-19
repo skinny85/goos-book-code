5c7faee6ad071f26b6312525aa533040907caa94
Adam Ruka <adamruka85@gmail.com>
2016-01-19 23:40:19 +0100

    Adding items through the user interface - Red (updating the tests, page 184).

--- src/test/java/test/endtoend/auctionsniper/ApplicationRunner.java
+++ src/test/java/test/endtoend/auctionsniper/ApplicationRunner.java
@@ -15,11 +15,21 @@ public class ApplicationRunner {
     private AuctionSniperDriver driver;
 
     public void startBiddingIn(final FakeAuctionServer... auctions) {
+        startSniper();
+
+        for (FakeAuctionServer auction : auctions) {
+            String itemId = auction.getItemId();
+            driver.startBiddingFor(itemId);
+            driver.showsSniperStatus(itemId, 0, 0, SnipersTableModel.textFor(JOINING));
+        }
+    }
+
+    private void startSniper() {
         Thread thread = new Thread("Test Application") {
             @Override
             public void run() {
                 try {
-                    Main.main(arguments(auctions));
+                    Main.main(XMPP_HOSTNAME, SNIPER_ID, SNIPER_PASSWORD);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -31,20 +41,6 @@ public class ApplicationRunner {
         driver = new AuctionSniperDriver(1000);
         driver.hasTitle(MainWindow.APPLICATION_TITLE);
         driver.hasColumnTitles();
-        for (FakeAuctionServer auction : auctions) {
-            driver.showsSniperStatus(auction.getItemId(), 0, 0, SnipersTableModel.textFor(JOINING));
-        }
-    }
-
-    private static String[] arguments(FakeAuctionServer... auctions) {
-        String[] arguments = new String[auctions.length + 3];
-        arguments[0] = XMPP_HOSTNAME;
-        arguments[1] = SNIPER_ID;
-        arguments[2] = SNIPER_PASSWORD;
-        for (int i = 0; i < auctions.length; i++) {
-            arguments[i + 3] = auctions[i].getItemId();
-        }
-        return arguments;
     }
 
     public void showsSniperHasLostAuction(FakeAuctionServer auction) {
--- src/test/java/test/endtoend/auctionsniper/AuctionSniperDriver.java
+++ src/test/java/test/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -2,11 +2,14 @@ package test.endtoend.auctionsniper;
 
 import auctionsniper.ui.MainWindow;
 import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.JButtonDriver;
 import com.objogate.wl.swing.driver.JFrameDriver;
 import com.objogate.wl.swing.driver.JTableDriver;
 import com.objogate.wl.swing.driver.JTableHeaderDriver;
+import com.objogate.wl.swing.driver.JTextFieldDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 
+import javax.swing.*;
 import javax.swing.table.JTableHeader;
 
 import static com.objogate.wl.swing.matcher.IterableComponentsMatcher.matching;
@@ -30,6 +33,23 @@ public class AuctionSniperDriver extends JFrameDriver {
                 withLabelText("Last Price"),
                 withLabelText("Last Bid"),
                 withLabelText("State")));
+    }
+
+    @SuppressWarnings("unchecked")
+    public void startBiddingFor(String itemId) {
+        itemIdField().replaceAllText(itemId);
+        bidButton().click();
+    }
+
+    private JTextFieldDriver itemIdField() {
+        JTextFieldDriver newItemId =
+                new JTextFieldDriver(this, JTextField.class, named(MainWindow.NEW_ITEM_ID_NAME));
+        newItemId.focusWithMouse();
+        return newItemId;
+    }
+
+    private JButtonDriver bidButton() {
+        return new JButtonDriver(this, JButton.class, named(MainWindow.JOIN_BUTTON_NAME));
     }
 
     public void showsSniperStatus(String itemId, String statusText) {
