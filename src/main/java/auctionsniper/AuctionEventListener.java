c98c25ab1cbbb8e0182efddea5e1bcbdf9bd499b
Adam Ruka <adamruka85@gmail.com>
2016-01-20 23:13:14 +0100

    Refactoring Main - using Announcer<AuctionEventListener> as a listener for AuctionMessageTranslator instead of AuctionSniper ("Isolating the chat", page 193).

--- src/main/java/auctionsniper/AuctionEventListener.java
+++ src/main/java/auctionsniper/AuctionEventListener.java
@@ -1,6 +1,8 @@
 package auctionsniper;
 
-public interface AuctionEventListener {
+import java.util.EventListener;
+
+public interface AuctionEventListener extends EventListener {
     enum PriceSource {
         FromSniper, FromOtherBidder;
     }
