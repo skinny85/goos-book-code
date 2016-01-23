e76daa4a9498bde3b2586bd961fdbe667564a2c9
Adam Ruka <adamruka85@gmail.com>
2016-01-23 19:48:07 +0100

    Move SwingThreadSniperListener to a top-level class (should have been done a long time ago).

--- src/main/java/auctionsniper/Main.java
+++ src/main/java/auctionsniper/Main.java
@@ -51,20 +51,4 @@ public class Main{
         });
     }
 
-    public static class SwingThreadSniperListener implements SniperListener {
-        private final SniperListener sniperListener;
-
-        public SwingThreadSniperListener(SniperListener sniperListener) {
-            this.sniperListener = sniperListener;
-        }
-
-        @Override
-        public void sniperStateChanged(final SniperSnapshot snapshot) {
-            SwingUtilities.invokeLater(new Runnable() {
-                public void run() {
-                    sniperListener.sniperStateChanged(snapshot);
-                }
-            });
-        }
-    }
 }
