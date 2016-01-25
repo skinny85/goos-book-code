570d0cdfcd048a90695f158e14956ac7edea3b08
Adam Ruka <adamruka85@gmail.com>
2016-01-26 00:10:57 +0100

    Handling failure, AuctionSniper unit test - Red ("Displaying the Failure", page 218).

--- src/test/java/test/auctionsniper/AuctionSniperTest.java
+++ src/test/java/test/auctionsniper/AuctionSniperTest.java
@@ -158,6 +158,17 @@ public class AuctionSniperTest {
         sniper.currentPrice(price, increment, PriceSource.FromOtherBidder);
     }
 
+    @Test
+    public void reportsFailedIfAuctionFailsWhenBidding() {
+        ignoringAuction();
+        allowingSniperBidding();
+
+        expectSniperToFailWhenItIs("bidding");
+
+        sniper.currentPrice(123, 45, PriceSource.FromOtherBidder);
+        sniper.auctionFailed();
+    }
+
     private Matcher<SniperSnapshot> aSniperThatIs(final SniperState state) {
         return new FeatureMatcher<SniperSnapshot, SniperState>(
                 equalTo(state), "sniper that is ", "was") {
@@ -177,6 +188,18 @@ public class AuctionSniperTest {
     private void allowingSniperWinning() {
         context.checking(new Expectations() {{
             allowing(sniperListener).sniperStateChanged(with(aSniperThatIs(WINNING))); then(sniperState.is("winning"));
+        }});
+    }
+
+    private void ignoringAuction() {
+        context.checking(new Expectations() {{
+            ignoring(auction);
+        }});
+    }
+
+    private void expectSniperToFailWhenItIs(final String state) {
+        context.checking(new Expectations() {{
+            atLeast(1).of(sniperListener).sniperStateChanged(new SniperSnapshot(ITEM_ID, 0, 0, SniperState.FAILED)); when(sniperState.is(state));
         }});
     }
 }
